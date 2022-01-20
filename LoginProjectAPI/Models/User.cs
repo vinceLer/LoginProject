bb340c777c9a186f335e187dd2f2b9dc89d3e35b
vince <vince.leroy@outlook.com>
2022-01-20 23:28:30 +0100

    LoginController (empty) & Models/User (set up) added

---
+++ LoginProjectAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LoginProjectAPI.Models
+{
+    public class User
+    {
+        private int id;
+        private string username;
+        private string mail;
+        private string password;
+
+        public int Id { get => id; set => id = value; }
+        public string Username { get => username; set => username = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
